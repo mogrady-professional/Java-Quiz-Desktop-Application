61c408c78bec767d29f62f9950f58b39bc2d586c
Michael O'Grady <mogrady.professional@gmail.com>
2022-03-25 12:56:22 +0000

    Initial Commit

---
+++ src/javaQuiz/Quiz.java
@@ -0,0 +1,35 @@
+package javaQuiz;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+// Action listener -> 
+public class Quiz implements ActionListener {
+
+//	Constructor
+	public Quiz() {
+
+	}
+
+//	Method to move to next qustion
+	public void nextQuestion() {
+
+	}
+
+//	Method Button Clicking
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+	
+//	Method for Display answer
+	public void displayAnswer() {
+		
+	}
+	
+//	Method for Final Results
+	public void results() {
+		
+	}
+}
---
+++ src/javaQuiz/Runner.java
@@ -0,0 +1,8 @@
+package javaQuiz;
+
+public class Runner {
+	public static void main(String[] args) {
+//		Create new instance of Quiz
+		Quiz q = new Quiz();
+	}
+}
